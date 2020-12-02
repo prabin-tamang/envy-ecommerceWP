ad3203ec26d156052700f6833b9238d7b34ee575
prabin-tamang <prabin96@hotmail.co.uk>
2020-12-02 22:49:14 +0000

    added font stacks

---
+++ template-contactus.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+Template Name: Contact Us
+*/
+
+?>
+
+<?php get_header('secondary');  ?>
+
+<main>
+    <div class="container">
+        <h1><?php the_title(); ?></h1>
+        <p>This is the contact us page.</p>
+    </div>
+</main>
+
+
+
+<?php get_footer(); ?>
