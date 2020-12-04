117f55a1333b009b2657a69263a730275cf3e7ad
prabin-tamang <prabin96@hotmail.co.uk>
2020-12-04 14:55:19 +0000

    added 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header('secondary'); ?>
+
+<div class="container">
+    <main>   
+        <div class="error-page">    
+        <h1>404 page not found</h1>
+        <p>The page you requested does not exist. Click <a href="<?php echo get_term_link(9)?>">here</a> to continue shopping.</p>
+        </div>
+    </main>
+</div>
+
+
+<?php get_footer(); ?>
